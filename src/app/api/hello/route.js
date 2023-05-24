ea391c72a68f9598f3d140544b6c082de38293ab
krawczyk071 <krawczyk071@gmail.com>
2023-05-24 13:28:09 +0200

    progress bar

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,29 @@
 export async function GET(request) {
   return new Response("Hello NextJS");
 }
+
+// export const GET = async (request) => {
+//   try {
+//       await connectToDB()
+
+//       const prompts = await Prompt.find({}).populate('creator')
+
+//       return new Response(JSON.stringify(prompts), { status: 200 })
+//   } catch (error) {
+//       return new Response("Failed to fetch all prompts", { status: 500 })
+//   }
+// }
+
+// export const POST = async (request) => {
+//   const { userId, prompt, tag } = await request.json();
+
+//   try {
+//       await connectToDB();
+//       const newPrompt = new Prompt({ creator: userId, prompt, tag });
+
+//       await newPrompt.save();
+//       return new Response(JSON.stringify(newPrompt), { status: 201 })
+//   } catch (error) {
+//       return new Response("Failed to create a new prompt", { status: 500 });
+//   }
+// }
--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -5,45 +5,52 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 // card 4242 4242 4242 4242
 
 export async function POST(request) {
-  const jsonValue = await request.json();
-  const origin = request.headers.get("origin");
-  const params = {
-    submit_type: "pay",
-    mode: "payment",
-    payment_method_types: ["card"],
-    billing_address_collection: "auto",
-    shipping_options: [{ shipping_rate: "shr_1NBCgGAlVPvHxSVQhTOO2Gsy" }],
-    line_items: jsonValue.map((item) => {
-      //   const newImage = urlForImage(item.images[0]);
-      const img = item.images[0].asset._ref;
-      const newImage = img
-        .replace("image-", "https://cdn.sanity.io/images/7t28rzgo/production/")
-        .replace("-webp", ".webp")
-        .replace("-jpg", ".jpg");
+  try {
+    const jsonValue = await request.json();
+    const origin = request.headers.get("origin");
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1NBCgGAlVPvHxSVQhTOO2Gsy" }],
+      line_items: jsonValue.map((item) => {
+        //   const newImage = urlForImage(item.images[0]);
+        const img = item.images[0].asset._ref;
+        const newImage = img
+          .replace(
+            "image-",
+            "https://cdn.sanity.io/images/7t28rzgo/production/"
+          )
+          .replace("-webp", ".webp")
+          .replace("-jpg", ".jpg");
 
-      return {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: item.name,
-            images: [newImage],
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.name,
+              images: [newImage],
+            },
+            unit_amount: parseInt(item.price * 100),
           },
-          unit_amount: parseInt(item.price * 100),
-        },
-        adjustable_quantity: {
-          enabled: true,
-          minimum: 1,
-        },
-        quantity: item.qty,
-      };
-    }),
-    success_url: `${origin}/confirm`,
-    cancel_url: `${origin}/canceled`,
-  };
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+          },
+          quantity: item.qty,
+        };
+      }),
+      success_url: `${origin}/confirm`,
+      cancel_url: `${origin}/canceled`,
+    };
 
-  // Create Checkout Sessions from body params.
-  const session = await stripe.checkout.sessions.create(params);
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create(params);
 
-  // res.status(200).json(session);
-  return NextResponse.json(session);
+    // res.status(200).json(session);
+    return NextResponse.json(session);
+  } catch (error) {
+    return new Response("Payment failed", { status: 500 });
+  }
 }
